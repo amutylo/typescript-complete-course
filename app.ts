e1c35b696c0f99db67a16bcd9ca6f6548b58f94a
Andrii Mutylo <amutylo@webcodin.com>
2019-10-08 10:01:06 +0300

    added gitignore and changes in app

--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-console.log("It works!");
+let hobbies = ['cooking', 'swimming'];
+// console.log(typeof hobbies);
+
+// type aliases
+
+type Complex = { data: number[]; output: (all: boolean) => number[] };
+
+let complex: Complex = {
+	data: [10, 12, 14],
+	output: function(all: boolean): number[] {
+		return this.data;
+	},
+};
+
+console.log(complex.output(true));
